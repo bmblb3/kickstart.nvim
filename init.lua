2313b79e9defc355c67931b9a80f25b8bb532d6b
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2024-11-02 18:29:32 +0100

    feat: enable formatters for python, typst, sh, yaml, javascript

--- init.lua
+++ init.lua
@@ -700,10 +700,13 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
+        typst = { 'typstfmt' },
+        sh = { 'shellharden' },
+        yaml = { 'yamlfmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
