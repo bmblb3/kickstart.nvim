232d17afcb345b138e59060cd5ecd481238ffde1
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2024-11-23 06:55:50 +0100

    feat: add prettierd for html

--- init.lua
+++ init.lua
@@ -720,6 +720,7 @@ require('lazy').setup({
         typst = { 'typstfmt' },
         sh = { 'shellharden' },
         yaml = { 'yamlfmt' },
+        html = { 'prettierd' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
