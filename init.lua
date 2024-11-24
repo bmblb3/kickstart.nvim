66259aa67e193348867124b9a4837da338ec4e20
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2024-11-24 08:30:01 +0100

    feat: add vim-test

--- init.lua
+++ init.lua
@@ -946,6 +946,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    'vim-test/vim-test',
+    config = function()
+      vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' })
+      vim.keymap.set('n', '<leader>tc', ':TestClass<CR>', { desc = '[T]est [C]lass' })
+      vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
+      vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { desc = '[T]est [S]uite' })
+      vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' })
+      vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>', { desc = '[T]est [V]isit' })
+    end,
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
