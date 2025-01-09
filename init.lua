a60c74c8a85b792bb24546ec6246de9d1a2ca4dd
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2025-01-09 21:47:24 +0100

    feat: add formatter for json

--- init.lua
+++ init.lua
@@ -731,6 +731,7 @@ require('lazy').setup({
         sh = { 'shellharden' },
         yaml = { 'yamlfmt' },
         html = { 'prettierd' },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
